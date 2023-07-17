c900080473ac61e6c5926efc79d6ba0af4017c0f
Arthur de Camargo Alves <arthur_rokero@hotmail.com>
2023-07-17 16:54:06 -0300

    Update Home

--- projetoFinal/aplicativo/aplicativo/ContentView.swift
+++ projetoFinal/aplicativo/aplicativo/ContentView.swift
@@ -14,19 +14,25 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Cor do fundo
-            LinearGradient(colors: [.blue, .black], startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [.white, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
             
             VStack {
                 Text("SoberCheck")
                     .font(.system(size: 45, design: .rounded))
                     .fontWeight(.black)
                     .offset(y: 60)
+                    .foregroundStyle(.purple)
+                    
                 
                 Spacer()
                 
                 ZStack {
                     Circle()
-                        .fill(Color(foiMedido ? .green : .brown))
+                        .fill(Color(foiMedido ? .green : .white))
+                        .overlay(
+                                    RoundedRectangle(cornerRadius: 100)
+                                        .stroke(Color.purple, lineWidth: 5)
+                                )
                         .frame(width: 200, height: 200)
                         .onTapGesture {
                             withAnimation(.easeInOut(duration: 1.0)) {
@@ -43,6 +49,22 @@ struct ContentView: View {
                 } // ZStack
 
                 Spacer()
+                TabView {
+                    HomeView()
+                        .badge(2)
+                        .tabItem {
+                            Label("HomeKit", systemImage: "tray.and.arrow.down.fill")
+                        }
+                    HistoricoView()
+                        .tabItem {
+                            Label("clock.arrow.circlepath", systemImage: "tray.and.arrow.up.fill")
+                        }
+                    ConfigView()
+                        .badge("gear")
+                        .tabItem {
+                            Label("Account", systemImage: "person.crop.circle.fill")
+                        }
+                }
             } // VStack
             
         } // ZStack
